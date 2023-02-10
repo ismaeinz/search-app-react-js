44c3a1270581d80b38783f93a32352a2a4610e15
ismaeinz <ismaeinqasem@gmail.com>
2023-02-10 16:08:55 +0200

    simple search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import JSONDATA from "./MOCK_DATA.json";
 
 const App = () => {
-  return <div></div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (;
+    <>
+      <div>
+        <input
+          type="text "
+          placeholder="Search"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+
+        {JSONDATA.filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return val;
+          }
+        })
+          //
+          .map(
+            (val) => {
+              return (
+                <div className="filter">
+                  <p className="filter-name">{val.first_name}</p>
+                </div>
+              );
+            }
+            //
+          )}
+      </div>
+    </>
+  );
 };
 
 export default App;
